759f5e9f53d7ed38e358bf2a3bd53198828c7dd5
Zurab Zurabishvili <accs.zukazurabishvili@gmail.com>
2023-02-05 21:35:15 +0400

    add home page

---
+++ src/components/Homepage/Homepage.jsx
@@ -0,0 +1,19 @@
+import "./Homepage.css";
+
+const Homepage = () => {
+  return (
+    <div>
+      <img
+        src="src\components\Homepage\items\LOGO-02 3.png"
+        className="logo"
+      ></img>
+      <img src="src\components\Homepage\items\shutterstock_2189773743 1.png"></img>
+      <div className="line"></div>
+      <button className="addButton">
+        <div className="button-text">რეზიუმეს დამატება</div>
+      </button>
+    </div>
+  );
+};
+
+export default Homepage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Homepage from './components/Homepage/Homepage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Homepage></Homepage>
   </React.StrictMode>,
 )
