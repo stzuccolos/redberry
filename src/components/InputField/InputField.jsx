0666499bb03ed7eef822e4c0eaa11519a4321f93
Zurab Zurabishvili <accs.zukazurabishvili@gmail.com>
2023-02-06 16:22:30 +0400

    add input box with validation

--- src/components/InputField/InputField.jsx
+++ src/components/InputField/InputField.jsx
@@ -1,10 +1,32 @@
-import "./InputFieldInputField.css";
+import { useRef } from "react";
+import "./InputField.css";
 
-const InputField = () => {
+const InputField = (params) => {
+  const inputBox = useRef(null);
+
+  function Vaildate() {
+    const { pattern } = params;
+    if(!pattern){
+      return;
+    }
+
+    const inputText = inputBox.current.value;
+    const isMatch = pattern.test(inputText);
     
+    const classes = inputBox.current.classList;
+    if(isMatch){
+      classes.remove("invalid");
+      classes.add("valid");
+    }
+    else{
+      classes.remove("valid");
+      classes.add("invalid");
+    }
+  }
 
+  return (
+    <input ref={inputBox} onKeyUp={Vaildate} className="input-field"></input>
+  );
+};
 
-    return (
-        <input className="input-field"></input>
-    )
-}
+export default InputField;
